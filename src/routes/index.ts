0027d4a239d8a1e70aa5214b1380d252f46f45dc
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-03-30 11:25:12 +0300

    create roter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express"
 import { getAllCashiers, getCashierById, createCashier, updateCashier,removeCashier} from '../controllers/index'
 const router = Router()
 
+
 router.get('/cashier', getAllCashiers)
 router.get('/cashier/:id', getCashierById )
 router.post('/cashier', createCashier )
 router.put('/cashier/:id', updateCashier)
 router.delete('/cashier/:id', removeCashier)
+
 export default router
